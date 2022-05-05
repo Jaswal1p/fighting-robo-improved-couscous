9a52017d5e4b889227b1f411423aafa7f6ceba96
Jaswal1p <Jaswal1p@yahoo.com>
2022-05-05 16:26:21 -0400

    added function for player wants to fight or not

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,10 +1,131 @@
+// Game functions
+
+
+var randomNumber = function(min, max) {
+  var value = Math.floor(Math.random() * (max - min) + min);
+
+  return value;
+};
+
+
+var fightOrSkip = function() {
+  // ask player if they'd like to fight or run
+  var promptFight = window.prompt('Would you like to FIGHT or SKIP this battle? Enter "FIGHT" or "SKIP" to choose.');
+
+  // validate prompt answer
+  if (promptFight === "" || promptFight === null) {
+    window.alert("You need to provide a valid answer! Please try again.");
+    
+    return fightOrSkip();
+  }
+
+  // convert promptFight to all lowercase so we can check with less options
+  promptFight = promptFight.toLowerCase();
+
+  if (promptFight === "skip") {
+    
+    var confirmSkip = window.confirm("Are you sure you'd like to quit?");
+
+    
+        if (confirmSkip) {
+          window.alert(playerInfo.name + " has decided to skip this fight. Goodbye!");
+          // subtract money from playerMoney for skipping, but don't let them go into the negative
+          playerInfo.money = Math.max(0, playerInfo.money - 10);
+          
+
+          
+          return true;
+        }
+  }
+  return false;
+};
+
+
+
+var fight = function(enemy) {
+  // keep track of who goes first
+  var isPlayerTurn = true;
+
+  // randomly change turn order
+  if (Math.random() > 0.5) {
+    isPlayerTurn = false;
+  }
+
+  while (playerInfo.health > 0 && enemy.health > 0) {
+    if (isPlayerTurn) {
+      // ask player if they'd like to fight or skip using fightOrSkip function
+          if (fightOrSkip()) {
+            // if true, leave fight by breaking loop
+            break;
+          }
+
+      var damage = randomNumber(playerInfo.attack - 3, playerInfo.attack);
+
+      // remove enemy's health by subtracting the amount we set in the damage variable
+          enemy.health = Math.max(0, enemy.health - damage);
+          console.log(
+            playerInfo.name +
+              " attacked " +
+              enemy.name +
+              ". " +
+              enemy.name +
+              " now has " +
+              enemy.health +
+              " health remaining."
+          );
+
+      // check enemy's health
+      if (enemy.health <= 0) {
+        window.alert(enemy.name + " has died!");
+
+        // award player money for winning
+        playerInfo.money = playerInfo.money + 20;
+
+        // leave while() loop since enemy is dead
+        break;
+      } else {
+        window.alert(enemy.name + " still has " + enemy.health + " health left.");
+      }
+      // player gets attacked first
+    } else {
+      var damage = randomNumber(enemy.attack - 3, enemy.attack);
+
+      // remove enemy's health by subtracting the amount we set in the damage variable
+      playerInfo.health = Math.max(0, playerInfo.health - damage);
+      console.log(
+        enemy.name +
+          " attacked " +
+          playerInfo.name +
+          ". " +
+          playerInfo.name +
+          " now has " +
+          playerInfo.health +
+          " health remaining."
+      );
+
+// check player's health
+if (playerInfo.health <= 0) {
+  window.alert(playerInfo.name + " has died!");
+  // leave while() loop if player is dead
+  break;
+} else {
+  window.alert(playerInfo.name + " still has " + playerInfo.health + " health left.");
+}
+}
+// switch turn order for next round
+isPlayerTurn = !isPlayerTurn;
+}
+};
+
+
+
 var playerName = window.prompt("What is your robot's name?");
 var playerHealth = 100;
 var playerAttack = 10;
 var playerMoney = 10;
 
 
-// You can also log multiple values at once like this
+
 console.log(playerName, playerAttack, playerHealth);
 
 var enemyName = "Roborto";
@@ -13,10 +134,10 @@ var enemyAttack = 12;
 
 // fight function
 var fight = function() {
-  // Alert players that they are starting the round
+  
   window.alert("Welcome to Robot Gladiators!");
 
-  // ask player if they'd like to fight or run
+  
   var promptFight = window.prompt('Would you like to FIGHT or SKIP this battle? Enter "FIGHT" or "SKIP" to choose.');
 
   // if player choses to fight, then fight
